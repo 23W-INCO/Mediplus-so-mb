923174a6c967d5ebdb08a2617ffc0399524a7720
s-mbk <somtochukwumbuko@gmail.com>
2024-01-20 06:17:56 +0100

    Updated data-sanitizer.py

--- ndde-d3/data-sanitizer.py
+++ ndde-d3/data-sanitizer.py
@@ -1,7 +1,18 @@
+# WARNING: DO NOT RUN THIS SCRIPT UNLESS ABSOLUTELY NECESSARY
+# This script is designed for data sanitizing and may create new files 
+# in the project directory. Running this script can affect the files 
+# referred to in the project and potentially alter the project's data structure.
+# IMPORTANT: Portions of this script that create new files are currently commented out.
+# To enable file creation, carefully review and uncomment these sections of the code.
+# Proceed with caution and ensure you have a backup of your data before running this script.
+
+
 import pandas as pd
 import pycountry
 
-csv_file_path = 'natural-disasters-decadal-raw.csv'
+
+# Snippet for removing unsupported entities and adding codes to unrecognized countries 
+csv_file_path = './csv/archive/natural-disasters-decadal-raw.csv'
 
 def get_country_code(country_name, custom_mappings):
     if country_name in custom_mappings:
@@ -11,7 +22,6 @@ def get_country_code(country_name, custom_mappings):
         return pycountry.countries.get(name=country_name).alpha_3
     except:
         return None
-
 
 def add_country_codes(csv_file_path, output_file_path):
     # Custom mappings for unrecognized countries
@@ -70,9 +80,106 @@ def add_country_codes(csv_file_path, output_file_path):
     df.to_csv(output_file_path, index=False)
 
 
-# Run
+# Uncomment to Run
 # output_file_path = 'natural-disasters-decadal.csv'
 # add_country_codes(csv_file_path, output_file_path)
+
+
+
+# Snippet for adding row with HXL hashtags to enforce HXL-compliance
+# HXL hashtags for specific columns
+hxl_hashtags = {
+    'Country name': '#country+name',
+    'Country code': '#country+code',
+    'Year': '#date+year',
+
+    # all disasters
+    'Number of deaths from disasters': '#affected+dead+disasters',
+    'Number of people injured from disasters': '#affected+injured+disasters',
+    'Number of total people affected by disasters': '#affected+total+disasters',
+    'Number of people left homeless from disasters': '#affected+homeless+disasters',
+    'Total economic damages from disasters as a share of GDP': '#impact+econ+gdp+disasters',
+    
+    # droughts
+    'Number of deaths from drought': '#affected+dead+drought',
+    'Number of people injured from drought': '#affected+injured+drought',
+    'Number of total people affected by drought': '#affected+total+drought',
+    'Number of people left homeless from drought': '#affected+homeless+drought',
+    'Total economic damages from drought as a share of GDP': '#impact+econ+gdp+drought',
+
+    # earthquakes
+    'Number of deaths from earthquakes': '#affected+dead+earthquake',
+    'Number of people injured from earthquakes': '#affected+injured+earthquake',
+    'Number of total people affected by earthquakes': '#affected+total+earthquake',
+    'Number of people left homeless from earthquakes': '#affected+homeless+earthquake',
+    'Total economic damages from earthquakes as a share of GDP': '#impact+econ+gdp+earthquake',
+
+    # volcanoes
+    'Number of deaths from volcanic activity': '#affected+dead+volcanic',
+    'Number of people injured from volcanic activity': '#affected+injured+volcanic',
+    'Number of total people affected by volcanic activity': '#affected+total+volcanic',
+    'Number of people left homeless from volcanic activity': '#affected+homeless+volcanic',
+    'Total economic damages from volcanic activity as a share of GDP': '#impact+econ+gdp+volcanic',
+
+    # floods
+    'Number of deaths from floods': '#affected+dead+floods',
+    'Number of people injured from floods': '#affected+injured+floods',
+    'Number of total people affected by floods': '#affected+total+floods',
+    'Number of people left homeless from floods': '#affected+homeless+floods',
+    'Total economic damages from floods as a share of GDP': '#impact+econ+gdp+floods',
+
+    # mass movements
+    'Number of deaths from mass movements': '#affected+dead+massmovement',
+    'Number of people injured from mass movements': '#affected+injured+massmovement',
+    'Number of total people affected by mass movements': '#affected+total+massmovement',
+    'Number of people left homeless from mass movements': '#affected+homeless+massmovement',
+    'Total economic damages from mass movements as a share of GDP': '#impact+econ+gdp+massmovement',
+
+    # storms
+    'Number of deaths from storms': '#affected+dead+storms',
+    'Number of people injured from storms': '#affected+injured+storms',
+    'Number of total people affected by storms': '#affected+total+storms',
+    'Number of people left homeless from storms': '#affected+homeless+storms',
+    'Total economic damages from storms as a share of GDP': '#impact+econ+gdp+storms',
+
+    # landslides
+    'Number of deaths from landslides': '#affected+dead+landslide',
+    'Number of people injured from landslides': '#affected+injured+landslide',
+    'Number of total people affected by landslides': '#affected+total+landslide',
+    'Number of people left homeless from landslides': '#affected+homeless+landslide',
+    'Total economic damages from landslides as a share of GDP': '#impact+econ+gdp+landslide',
+
+    # wildfires
+    'Number of deaths from wildfires': '#affected+dead+fire',
+    'Number of people injured from wildfires': '#affected+injured+fire',
+    'Number of total people affected by wildfires': '#affected+total+fire',
+    'Number of people left homeless from wildfires': '#affected+homeless+fire',
+    'Total economic damages from wildfires as a share of GDP': '#impact+econ+gdp+fire',
+
+    # extreme temperatures
+    'Number of deaths from extreme temperatures': '#affected+dead+temperature',
+    'Number of people injured from extreme temperatures': '#affected+injured+temperature',
+    'Number of total people affected by extreme temperatures': '#affected+total+temperature',
+    'Number of people left homeless from extreme temperatures': '#affected+homeless+temperature',
+    'Total economic damages from extreme temperatures as a share of GDP': '#impact+econ+gdp+temperature'
+}
+
+csv_file_path_2 = './csv/archive/natural-disasters-decadal.csv'
+
+# Read the CSV file
+df = pd.read_csv(csv_file_path_2)
+
+# Uncomment to Run
+# Create a new row for HXL hashtags
+# hxl_row = [hxl_hashtags.get(col, '') for col in df.columns]
+
+# Insert the row of HXL hashtags at the second position (index 1)
+# df.loc[-1] = hxl_row  # add a row at the end of the DataFrame
+# df.index = df.index + 1  # shift the index
+# df = df.sort_index()  # sort by index
+
+# Write to a new CSV file
+# df.to_csv('hxl-compliant-' + csv_file_path_2 + '-data', index=False)
 
 
 
@@ -90,6 +197,6 @@ def create_decades_array(csvData):
 
     return decades
 
-# Run
-decades = create_decades_array(csv_file_path)
-print(decades)
+# Uncomment to Run
+# decades = create_decades_array(csv_file_path)
+# print(decades)
