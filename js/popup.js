3ba79ff0ab4a51c450433d2d1bb5af3fcd9f83d4
s-mbk <somtochukwumbuko@gmail.com>
2024-01-26 08:03:25 +0100

    Updated code to close popup when ESC key is pressed

--- js/popup.js
+++ js/popup.js
@@ -1,32 +1,38 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var modal = document.getElementById("popup-modal");
-    var closeButton = document.querySelector(".close-button");
-  
-    function toggleModal() {
-      modal.style.display = "block";
-    }
-  
-    function openInNewTab(url) {
-      window.open(url, '_blank').focus();
-    }
-  
-    var exploreButton = document.querySelector(".modal-content button");
-    exploreButton.addEventListener("click", function() {
-      openInNewTab('NDDE/index.html');
-    });
-  
-    function windowOnClick(event) {
-      if (event.target === modal) {
-        modal.style.display = "none";
-      }
-    }
-  
-    closeButton.addEventListener("click", function() {
-      modal.style.display = "none";
-    });
-  
-    window.addEventListener("click", windowOnClick);
-  
-    setTimeout(toggleModal, 5000); // time delay before popup appears
+document.addEventListener("DOMContentLoaded", function () {
+  var modal = document.getElementById("popup-modal");
+  var closeButton = document.querySelector(".close-button");
+
+  function toggleModal() {
+    modal.style.display = "block";
+  }
+
+  function openInNewTab(url) {
+    window.open(url, "_blank").focus();
+  }
+
+  var exploreButton = document.querySelector(".modal-content button");
+  exploreButton.addEventListener("click", function () {
+    openInNewTab("NDDE/index.html");
   });
-  
+
+  function windowOnClick(event) {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  }
+
+  function closeOnEscapeKey(event) {
+    if (event.key === "Escape") {
+      modal.style.display = "none";
+    }
+  }
+
+  closeButton.addEventListener("click", function () {
+    modal.style.display = "none";
+  });
+
+  window.addEventListener("click", windowOnClick);
+  window.addEventListener("keydown", closeOnEscapeKey);
+
+  setTimeout(toggleModal, 5000); // time delay before popup appears
+});
